7c5014ba02c5609feb99dc93c0403e0074d989e8
Dylan Hicks <dman1366@gmail.com>
2021-12-10 09:30:51 -0700

    Fucked this day up son :)

--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,8 +1,19 @@
 #![allow(unused_imports)]
+mod syntax_checker;
+
+use std::os::windows::fs::symlink_file;
+
 use crate::util::file_reader::to_string_vector;
+use syntax_checker::SyntaxChecker;
 
 pub fn run_day_10() {
-    // Uncomment
-    // let file_input = to_string_vector("inputs/day_10.txt").unwrap();
-    todo!()
+    let file_input = to_string_vector("inputs/day_10.txt").unwrap();
+
+    let checker = SyntaxChecker::new(&file_input);
+
+    let part_1_result = checker.get_total_error_score();
+    let part_2_result = checker.get_autocomplete_score();
+
+    println!("Day 10 Part 1: {}", part_1_result);
+    println!("Day 10 Part 2: {}", part_2_result);
 }
