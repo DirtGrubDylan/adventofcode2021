7b2c34e3e483a8547ed6dc898a06b7cff026ac4c
Dylan Hicks <dman1366@gmail.com>
2021-12-12 12:34:35 -0700

    Start day 12

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -1,8 +1,8 @@
 #![allow(unused_imports)]
+mod cave_system;
+
 use crate::util::file_reader::to_string_vector;
 
 pub fn run_day_12() {
-    // Uncomment
-    // let file_input = to_string_vector("inputs/day_12.txt").unwrap();
-    todo!()
+    let file_input = to_string_vector("inputs/day_12.txt").unwrap();
 }
